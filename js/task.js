ecc0bb3692c7e950a161b737b744cb3b986946a7
Polina <sunnyfur9@gmail.com>
2022-03-18 19:43:14 +0500

    first commit

---
+++ js/task.js
@@ -0,0 +1,83 @@
+// class Task {
+//     constructor(date, countNew, countInProcess) {
+//         this.date = date;
+//         this.countNew = countNew;
+//         this.countInProcess = countInProcess;
+//     }
+//     static GetListOfDates(listOfTasks, dateFrom, dateTo) {
+
+//         return Task.GetListOfCurrent(listOfTasks, dateFrom, dateTo).map(elem => elem.date);
+//     }
+//     static GetListOfNews(listOfTasks, dateFrom, dateTo) {
+//         return Task.GetListOfCurrent(listOfTasks, dateFrom, dateTo).map(elem => elem.countNew);
+//     }
+//     static GetListOfInProcess(listOfTasks, dateFrom, dateTo) {
+//         return Task.GetListOfCurrent(listOfTasks, dateFrom, dateTo).map(elem => [
+//             elem.countNew, elem.countInProcess + elem.countNew
+//         ]);
+//     }
+//     static GetListOfAll(listOfTasks, dateFrom, dateTo) {
+//         return Task.GetListOfCurrent(listOfTasks, dateFrom, dateTo).map(elem =>
+//             elem.countInProcess + elem.countNew);
+//     }
+
+//     static GetListOfCurrent(listOfTasks, dateFrom, dateTo) {
+//         if (!dateTo) {
+//             if (!dateFrom)
+//                 return listOfTasks.slice(-7);
+//             else
+//                 return listOfTasks.filter(date => new Date(date) >= new Date(dateFrom));
+//         }
+//         return listOfTasks.filter(date => new Date(date) >= new Date(dateFrom) && new Date(date) >= new Date(dateTo);
+//     }
+
+
+// }
+
+// const listOfTasks = [];
+// listOfTasks.push(new Task('28.02.22', 10, 0));
+// listOfTasks.push(new Task('01.03.22', 20, 30));
+// listOfTasks.push(new Task('02.03.22', 50, 10));
+// listOfTasks.push(new Task('03.03.22', 20, 20));
+// listOfTasks.push(new Task('04.03.22', 40, 10));
+// listOfTasks.push(new Task('05.03.22', 20, 10));
+// listOfTasks.push(new Task('06.03.22', 30, 20));
+// listOfTasks.push(new Task('07.03.22', 10, 0));
+// listOfTasks.push(new Task('08.03.22', 20, 30));
+// listOfTasks.push(new Task('09.03.22', 50, 10));
+// listOfTasks.push(new Task('10.03.22', 20, 20));
+// listOfTasks.push(new Task('11.03.22', 40, 10));
+// listOfTasks.push(new Task('12.03.22', 20, 10));
+// listOfTasks.push(new Task('13.03.22', 30, 20));
+// listOfTasks.push(new Task('14.03.22', 10, 0));
+// listOfTasks.push(new Task('15.03.22', 20, 30));
+// listOfTasks.push(new Task('16.03.22', 50, 10));
+// listOfTasks.push(new Task('17.03.22', 20, 20));
+// listOfTasks.push(new Task('18.03.22', 40, 10));
+// listOfTasks.push(new Task('19.03.22', 20, 10));
+// listOfTasks.push(new Task('20.03.22', 30, 20));
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(Task.GetListOfDates(listOfTasks));
+// const array = ["apple", "star2", "shamrock"];
+// // const array = ["apple", "star", "2"];
+// const newArr = [];
+// const newArr = array.reduce((newArr, curr) => newArr.concat([curr]), []);
+
+// array.forEach(x => newArr.unshift(x));
+// newArr.reverse();
+
+
+// array[0] = "dgdfg"
+// // for (let i; i < array.length; i++) newArr.push(array[i]);
+// console.log(newArr);
